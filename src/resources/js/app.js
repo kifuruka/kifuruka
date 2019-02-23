205712fb9ea78aee96e589cff9e67f566610f865
dan <idan3266@gmail.com>
2019-02-23 18:46:28 +0900

    first commit

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -5,29 +5,23 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+// require('./bootstrap');
 
-window.Vue = require('vue');
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router/';
+import store from './store/';
+import { sync } from 'vuex-router-sync';
+import VueProgressiveImage from 'vue-progressive-image'
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+Vue.use(VueProgressiveImage, {
+    blur: 30
+});
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+sync(store, router);
 
 const app = new Vue({
-    el: '#app'
-});
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app');
---
+++ src/resources/js/pages/TheTop.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    <header></header>
+  </main>
+</template>
+
+<script>
+import Header from "/components/Header";
+// import Footer from "../components/home/TopFooter";
+export default {
+  components: {
+    Header
+  }
+};
+</script>
