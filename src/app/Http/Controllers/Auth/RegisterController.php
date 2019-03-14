fafd7929ff262a4512ed836c6cfe18fca4a9d4a0
Takanori Ikegawa <>
2019-03-14 18:38:24 +0900

    login関係

--- src/app/Http/Controllers/Auth/RegisterController.php
+++ src/app/Http/Controllers/Auth/RegisterController.php
@@ -8,6 +8,10 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Auth\Events\Registered;
+
 class RegisterController extends Controller
 {
     /*
@@ -68,5 +72,18 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+    }
+
+    public function register(Request $request): JsonResponse
+    {
+        $validate = $this->validator($request->all());
+    
+        if ($validate->fails()) {
+            return new JsonResponse($validate->errors());
+        }
+    
+        event(new Registered($user = $this->create($request->all())));
+    
+        return new JsonResponse($user);
     }
 }
--- src/routes/api.php
+++ src/routes/api.php
@@ -16,3 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(["middleware" => "api"], function () {
+    Route::post('/register', 'Auth\RegisterController@register'); // 追加
+    // 認証が必要ないメソッド
+    Route::group(['middleware' => ['jwt.auth']], function () {
+    // 認証が必要なメソッド
+    });
+});
